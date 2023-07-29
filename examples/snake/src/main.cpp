325eefd5aa14b536c6c6569ddb96f9726a465d78
titfora <titouan821@gmail.com>
2023-07-29 15:15:28 +0200

    adjust parameters

--- examples/snake/src/main.cpp
+++ examples/snake/src/main.cpp
@@ -21,7 +21,7 @@ int main () {
     unsigned int maxIterationThresh = 500;
     int nbGame = 5;
     double bestFitness = 0.0;
-    while (bestFitness < 2500.0 && pop.getGeneration () < 10) { // while goal is not reach
+    while (bestFitness < 2500.0 && pop.getGeneration () < 150) { // while goal is not reach
         std::cout << "generation " << pop.getGeneration () << std::endl;
 
         for (unsigned int genomeId = 0; genomeId < popSize; genomeId ++) {
